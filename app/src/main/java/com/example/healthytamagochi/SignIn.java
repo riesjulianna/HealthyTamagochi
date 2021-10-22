f168a930d88e5112ef9f17687b5c5c5f2a1836db
Viktoria <viktoriabarta0202@gmail.com>
2021-10-22 16:22:01 +0200

    Kezdooldal+belepes form

---
+++ app/src/main/java/com/example/healthytamagochi/SignIn.java
@@ -0,0 +1,19 @@
+package com.example.healthytamagochi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
