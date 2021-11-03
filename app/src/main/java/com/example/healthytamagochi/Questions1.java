2d21cbcd281b4269d455476129c63c7230863534
Viktoria <viktoriabarta0202@gmail.com>
2021-11-03 15:44:49 +0100

    character select

---
+++ app/src/main/java/com/example/healthytamagochi/Questions1.java
@@ -0,0 +1,14 @@
+package com.example.healthytamagochi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Questions1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions1);
+    }
+}
